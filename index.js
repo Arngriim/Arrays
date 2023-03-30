aacd9dd25e9667d37c65ac72e04bfd30295d29a0
Carlos Augusto <krlosaugusto11@hotmail.com>
2023-03-29 22:20:07 -0300

    Arrays

---
+++ index.js
@@ -0,0 +1,13 @@
+ //let nota1 = 10
+ //let nota2 = 7
+ //let note3 = 6.5
+ //let nota4 = 7.5
+ //let nota5 = 8.5
+
+ const notas = [10, 7, 6.5, 7.5, 8.5]
+
+ // let media = (nota1 + nota2 + note3 + nota4 + nota5) / 5
+
+ let media = (notas[0] + notas[1] + notas[2] + notas[3] + notas[4]) / notas.length
+
+ console.log(media)
